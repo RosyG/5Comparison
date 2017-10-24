530a2a3be9b8c605b99772d2083be4f25db661f9
RosyG <rmga2106@gmail.com>
2017-10-24 15:19:16 -0500

    Previo commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 var comparacion=function(array1,array2){
-    document.write('Función declara, es una prueba');
+    if (array1.length !==array2.length){
+      return false
+    }else{
+      for (var i=0; i<array1.length; i++){
+        console.log(array1[i], array2[i])
+        if (array1[i] !==array2[i]){
+          document.write('Los arreglos son diferentes');
+          return false
+        }
+      }
+      document.write('Los arreglos son iguales');
+      return true;
+    }
 }
-}
+
 var arreglo1=[1,2,3,4,5,6,7,8,9,0];
 var arreglo2=[1,2,3,4,5,6,7,8,9,0];
 comparacion(arreglo1,arreglo2);
